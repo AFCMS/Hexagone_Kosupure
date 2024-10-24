ba2b35e980b7bd6f7fff1861024dca84105ca2c4
AFCMS <afcm.contact@gmail.com>
2024-10-24 15:56:45 +0200

    Fallback font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                "bangers": "Bangers",
+                "bangers": ["Bangers", "system-ui"],
             }
         },
     },
